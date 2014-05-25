6395e4eb3a065d0551ecf7744dbf284587e6e762
nomankhalid <noman.khalid@hotmail.com>
2014-05-25 20:17:11 +0500

    Solution to programming assignment
    The following code will solve the Programming Assignment 2 for R
    Programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## My function will create a matrix. It will calculate its inverse. Whenever the function is called, it will check if the
+## inverse of the matrix already exists. If it does it will directly show the inverse without computing it.
 
-## Write a short comment describing this function
-
+## The following code will create a matrix and cache it's inverse
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+
+## The following function computes the inverse of matrix and if previously calculated, it returns previously saved value
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
